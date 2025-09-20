6624da93465b3386839a9b5746c774a818273bca
Ahmad <ahmad.s.bugshan@gmail.com>
2025-09-20 19:52:32 +0300

    formated server.js

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-import http from 'node:http'
+import http from "node:http";
 
-const PORT = 8000
-
+const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-
-  if (req.url === "/api") {
-    res.end("This is from the server")
+  if (req.method === "GET") {
+    if (req.url === "/api") {
+      res.end("This is from the server");
+    } else {
+      res.end("Bad URL");
+    }
   }
-  else {
-    res.end("Bad URL")
-  }
+});
 
-})
-
-server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
